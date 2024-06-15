c3f8fdc31f5708b2890d1a4c388ca0de5f6db725
P Yaswanth Kumar <146411688+YaswanthKumar814@users.noreply.github.com>
2024-06-15 09:24:48 +0530

    Add files via upload

---
+++ SolarJS.js
@@ -0,0 +1,15 @@
+function createStars()
+{
+	const container = document.querySelector("body");
+	for(let i=0;i<2000;i++)
+	{
+		const star = document.createElement("div");
+		star.className="star";
+		star.style.width=".1px";
+		star.style.height=".1px";
+		star.style.top = Math.random() *100 + "%";
+		star.style.left = Math.random() *100 + "%";
+		container.appendChild(star);
+	}
+}
+createStars();
